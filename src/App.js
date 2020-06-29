090448182ea8b3b41a21c7957ed36cb6f8386b54
Anas452 <anasm9877@gmail.com>
2020-06-29 20:09:52 +0500

    login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from './Components/Login'
 
 function App() {
   return (
     <div className="App">
       <h1>COVID-19 TRACKER</h1>
+      <Login />
     </div>
   );
 }
